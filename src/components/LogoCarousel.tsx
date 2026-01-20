f1b9730b53b1fd7f4c6971dd81279fdfbd3b31d4
kazuki0205 <coltd.ichikabachika@gmail.com>
2026-01-20 21:38:49 +0900

    add: スライドショー

--- src/components/LogoCarousel.tsx
+++ src/components/LogoCarousel.tsx
@@ -2,18 +2,51 @@
 import styles from "./LogoCarousel.module.css";
 import Image from "next/image";
 
+const LOGOS = [
+  "/images/logos/logo1.png",
+  "/images/logos/logo2.png",
+  "/images/logos/logo3.png",
+  "/images/logos/logo4.png",
+  "/images/logos/logo5.png",
+  "/images/logos/logo6.png",
+  "/images/logos/logo7.png",
+  "/images/logos/logo8.png",
+  "/images/logos/logo9.png",
+  "/images/logos/logo10.png",
+];
+
 export default function LogoCarousel() {
-    return (
-        <section className={styles.section}>
-            <p className={styles.title}>導入企業・取引実績</p>
-            <div className={styles.carousel}>
-                <div className={styles.slide}>
-                    <Image src="/images/logo_set.png" alt="Client Logos" width={800} height={100} className={styles.logoImg} />
-                </div>
-                <div className={styles.slide}>
-                    <Image src="/images/logo_set.png" alt="Client Logos" width={800} height={100} className={styles.logoImg} />
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className={styles.section}>
+      <p className={styles.title}>導入企業・取引実績</p>
+      <div className={styles.carousel}>
+        {/* Original set */}
+        <div className={styles.slide}>
+          {LOGOS.map((src, index) => (
+            <Image
+              key={`logo-${index}`}
+              src={src}
+              alt={`Client Logo ${index + 1}`}
+              width={220}
+              height={100}
+              className={styles.logoImg}
+            />
+          ))}
+        </div>
+        {/* Duplicate set for infinite scroll */}
+        <div className={styles.slide}>
+          {LOGOS.map((src, index) => (
+            <Image
+              key={`logo-dup-${index}`}
+              src={src}
+              alt={`Client Logo ${index + 1}`}
+              width={220}
+              height={100}
+              className={styles.logoImg}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
